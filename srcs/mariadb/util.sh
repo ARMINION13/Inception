cafddc46528eb3b5ac4ef0737c5c4fcbade92c95
rgirondo <rgirondo@pop-os.localdomain>
2023-07-07 01:06:59 +0200

    mariadb conficurada con un error que no afecta pero que he ocultado por cuestiones esteticas, solo falta wordpress

--- srcs/mariadb/util.sh
+++ srcs/mariadb/util.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-su root service mysql start
+/etc/init.d/mysql start
+
+mysql -u root -e ""
+
+echo Creating database.
 
 mysql -e "CREATE DATABASE IF NOT EXISTS $DATABASE;"
 
+echo Creating new user.
+
 mysql -e "CREATE USER IF NOT EXISTS '${DATABASE_USER}' IDENTIFIED BY '${DATABASE_PASSWORD}';"
 
+echo Granting privileges to the new user.
+
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '${DATABASE_USER}' IDENTIFIED BY '${DATABASE_PASSWORD}';"
+
+echo Changing root password.
 
 mysql -e "FLUSH PRIVILEGES;"
 
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DATABASE_ROOT_PASSWORD';"
 
-mysqladmin --user=root --password=$DATABASE_ROOT_PASSWORD shutdown
+echo Stopping mysql service.
+
+mysql -u root --password=$DATABASE_ROOT_PASSWORD -e "SHUTDOWN;" > /dev/null 2> /dev/null
+
+sleep 2
+
+echo Starting mysqld_safe
 
 mysqld_safe
